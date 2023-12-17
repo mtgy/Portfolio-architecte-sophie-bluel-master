9f3050224f0b54d5d05dc114d7a1561dd4d993d3
mtgy <tanguy.marine@gmail.com>
2023-12-17 19:54:25 +0100

    etape 1.2 realisation des filtres

--- FrontEnd/assets/scripts.js
+++ FrontEnd/assets/scripts.js
@@ -1,43 +1,102 @@
+// Fonction de récupération des données du back
+function fetchData() {
+  fetch('http://localhost:5678/api/works/')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Erreur réseau');
+      }
+      return response.json(); // Transforme la réponse en JSON
+    })
+    .then(data => {
+      // Appel de la fonction pour afficher la galerie avec les données récupérées
+      itemGallery(data);
+      filtreBtns(data); // Appel pour créer les boutons de filtre
+    })
+    .catch(error => {
+      // Attrape les erreurs de requête ou de traitement des données
+      console.error('Erreur:', error);
+    });
+}
 
+// Fonction pour afficher la galerie avec les catégories et créer les boutons de filtre
+function itemGallery(data) {
+  const parentItemGallery = document.querySelector('.gallery');
 
-fetch('http://localhost:5678/api/works/')
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Erreur réseau');
-    }
-    return response.json(); // Transforme la réponse en JSON
-  })
-  .then(data => {
-    // Faire quelque chose avec les données récupérées
-    console.log(data);
-    itemGallery(data);
-  
-  })
-  .catch(error => {
-    // Attrape les erreurs de requête ou de traitement des données
-    console.error('Erreur:', error);
+  data.forEach(item => {
+    //creation des items 
+    const figureItemGallery = document.createElement('figure');
+    parentItemGallery.appendChild(figureItemGallery);
+
+    // integration des images
+    const imageItemGallery = document.createElement('img');
+    imageItemGallery.src = item.imageUrl;
+    imageItemGallery.setAttribute('alt', item.title);
+    figureItemGallery.appendChild(imageItemGallery);
+
+    // integration des captions
+    const captionItemGallery = document.createElement('figcaption');
+    captionItemGallery.innerHTML = item.title;
+    figureItemGallery.appendChild(captionItemGallery);
+
+    //integration des categories
+
+    figureItemGallery.setAttribute('data-category', item.category.name.toLowerCase());
+
   });
+}
+
+// Fonction pour créer les boutons de filtre
+function filtreBtns(data) {
+  const categories = new Set(data.map(item => item.category.name.toLowerCase()));
+  const filtersContainer = document.getElementById('filters');
 
 
-  function itemGallery(data) {
-    // Récupération de la div parent
-    let gallery = document.querySelector(".gallery");
-  
-    for (var i = 0; i < data.length; i++) {
-      // Création de la balise figure
-      let figureItemGallery = document.createElement("figure");
-      gallery.appendChild(figureItemGallery);
-  
-      // Création de la balise img
-      let imageItemGallery = document.createElement("img");
-      imageItemGallery.src = data[i].imageUrl;
-      // Attribution de l'attribut alt
-      imageItemGallery.setAttribute("alt", data[i].title);
-      figureItemGallery.appendChild(imageItemGallery);
-  
-      // Création de la balise caption
-      let captionItemGallery = document.createElement("figcaption");
-      captionItemGallery.innerHTML = data[i].title;
-      figureItemGallery.appendChild(captionItemGallery); // Attache la légende à la figure
-    }
+  // Création du bouton "Tous"
+  const btnTous = document.createElement('button');
+  btnTous.classList.add('filter-button');
+  btnTous.textContent = 'Tous';
+  filtersContainer.appendChild(btnTous);
+
+///action du btn tous
+  btnTous.addEventListener('click', function() {
+  const category = 'all'; // Définition de la catégorie pour le bouton "Tous"
+  filtreGallery(category);
+});
+
+// boutons categorie filtre
+  categories.forEach(category => {
+    const filtreBtn = document.createElement('button');
+    filtreBtn.classList.add('filter-button');
+    filtreBtn.setAttribute('data-category', category);
+    filtreBtn.textContent = category.charAt(0).toUpperCase() + category.slice(1);
+    filtersContainer.appendChild(filtreBtn);
+
+    filtreBtn.addEventListener('click', function() {
+      const category = this.getAttribute('data-category');
+       filtreGallery(category);
+    });
+    
+  });
+/// fonctionnement des boutons de filtre de la galerie
+  function filtreGallery(category) {
+    const figures = document.querySelectorAll('.gallery figure');
+    figures.forEach(figure => {
+      const figureCategory = figure.getAttribute('data-category');
+      if (category === 'all') {
+      figure.classList.remove('off');
+      } else {
+        if (figureCategory === category) {
+          figure.classList.remove('off');
+        } else {
+          figure.classList.add('off');
+        }
+      }
+    });
   }
+ 
+  
+  
+}
+
+// Appel de la fonction pour récupérer les données et afficher la galerie
+fetchData();
